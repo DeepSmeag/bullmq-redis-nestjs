1c2eaa2b525975393fb1f2c85afe4cf5f09888df
Cosmin <cosmin072@gmail.com>
2024-10-13 18:01:11 +0300

    cleaned unused temp code

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,20 +1,11 @@
-import { Controller, Get, Post, Sse } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService, JobRequest } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
   @Post()
   createRequest(): JobRequest {
     return this.appService.createRequest();
-  }
-  @Sse('events')
-  sseRequest() {
-    return this.appService.sseRequest();
   }
 }
--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -10,48 +10,15 @@ export type JobRequest = {
 @Injectable()
 export class AppService {
   constructor(private readonly requestQueueService: RequestQueueService) {}
-  // temporary array of requests
-  requests: JobRequest[] = [];
-
-  getHello(): string {
-    return JSON.stringify(this.requests);
-  }
 
   createRequest(): JobRequest {
     const newRequest: JobRequest = {
       id: uuidv4(),
       status: 'NOT STARTED',
     };
-    this.requests.push(newRequest);
     setTimeout(() => {
       this.requestQueueService.publishTask(newRequest.id);
     }, 200);
     return newRequest;
-  }
-  sseRequest(): Observable<MessageEvent> {
-    // choose a random status between the 4 possible
-    // ugly, but it's a temporary mock
-    // we have doubles to increase chance of waiting and in progress
-    return interval(1000).pipe(
-      map(
-        () =>
-          ({
-            data: {
-              status: [
-                'WAITING',
-                'WAITING',
-                'IN PROGRESS',
-                'IN PROGRESS',
-                'IN PROGRESS',
-                'IN PROGRESS',
-                'IN PROGRESS',
-                'IN PROGRESS',
-                'FINISHED',
-                'ERROR',
-              ][Math.floor(Math.random() * 10)],
-            },
-          }) as MessageEvent,
-      ),
-    );
   }
 }
